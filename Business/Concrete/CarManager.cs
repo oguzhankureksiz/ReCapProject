1265f82e778a0728490ad15fa5439c39cfda1d3f
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-08-06 17:46:30 +0300

    İş katmanı yapılandırılması yapıldı.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public IResult Add(Car car)
+        {
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
+        }
+
+        public IResult Delete(Car car)
+        {
+            _carDal.Delete(car);
+            return new SuccessResult(Messages.CarDeleted);
+        }
+
+        public IDataResult<Car> Get(int id)
+        {
+            return new SuccessDataResult<Car>(_carDal.Get(c=>c.CarId==id),Messages.CarListed);
+        }
+
+        public IDataResult<List<Car>> GetAll()
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarsListed);
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetail()
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(),Messages.CarListed);
+        }
+
+        public IResult Update(Car car)
+        {
+            _carDal.Update(car);
+            return new SuccessResult(Messages.CarUpdated);
+        }
+    }
+}
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string MaintenanceTime = "Service is maintenance.";
+
+        public static string CarAdded = "Car added.";
+        public static string CarDeleted = "Car deleted.";
+        public static string CarUpdated = "Car updated.";
+        public static string CarsListed = "Cars listed.";
+        public static string CarListed = "Car listed.";
+        public static string CarNameIsInvalid = "Car name is invalid.";
+
+
+        public static string BrandAdded = "Brand added.";
+        public static string BrandDeleted = "Brand deleted.";
+        public static string BrandUpdated = "Brand updated.";
+        public static string BrandsListed = "Brands listed.";
+        public static string BrandListed = "Brand listed.";
+
+        public static string ColorAdded = "Color added.";
+        public static string ColorDeleted = "Color deleted.";
+        public static string ColorUpdated = "Color updated.";
+        public static string ColorsListed = "Colors listed.";
+        public static string ColorListed = "Color listed.";
+
+        public static string CustomerAdded = "Customer added.";
+        public static string CustomerDeleted = "Customer deleted.";
+        public static string CustomerUpdated = "Customer updated.";
+        public static string CustomersListed = "Customers listed.";
+        public static string CustomerListed = "Customer listed.";
+
+        public static string UserAdded = "User added.";
+        public static string UserDeleted = "User deleted.";
+        public static string UserUpdated = "User updated.";
+        public static string UsersListed = "Users listed.";
+        public static string UserListed = "User listed.";
+
+        public static string RentalAdded = "Rental added.";
+        public static string RentalDeleted = "Rental deleted.";
+        public static string RentalUpdated = "Rental updated.";
+        public static string RentalsListed = "Rentals listed.";
+        public static string RentalListed = "Rental listed.";
+        public static string RentalNotAdded = "The vehicle has not been delivered yet.";
+    }
+}
