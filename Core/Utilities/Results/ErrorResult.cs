7502d9ecc6b75dc70cc6af6fd49825093bf68b11
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-08-06 14:44:03 +0300

    Core katmanında results yapılandırılması oluşturuldu.

---
+++ Core/Utilities/Results/ErrorResult.cs
@@ -0,0 +1,13 @@
+﻿using Core.Utilities.Results;
+
+public class ErrorResult : Result
+{
+    public ErrorResult(string message) : base(message, false)
+    {
+
+    }
+    public ErrorResult() : base(false)
+    {
+
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(string message,bool isSuccess):this(isSuccess)
+        {
+            Message = message;
+        }
+        public Result(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+        public string Message { get; }
+
+        public bool IsSuccess { get; }
+    }
+}
