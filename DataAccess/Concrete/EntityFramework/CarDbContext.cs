eb5456c8911c2260dccf329be41a6e466cf5a36c
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-08-06 14:17:36 +0300

    Nesnelerin veri erişim için gerekli olan yapılar oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/CarDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class CarDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-R06IT12\SQLEXPRESS;Database=RentCarDb;Trusted_Connection=True");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+    }
+}
